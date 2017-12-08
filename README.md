a44f32180bd769ae837bd4030f8a7bfa2688e5fd
kharazi <vahid@kharazi.net>
2017-12-08 01:20:11 +0000

    Add ride and README

---
+++ README.md
@@ -0,0 +1,35 @@
+# snapp-cli 
+
+
+[![asciicast](https://asciinema.org/a/4LMxOtcPPQDZQI8xpXdck1059.png)](https://asciinema.org/a/4LMxOtcPPQDZQI8xpXdck1059)
+
+privacy aware command-line interface to take an easy ride with Snapp like a boss.
+
+## Installation
+
+```
+$ cd mapscii
+$ npm install -g
+$ cd ..
+$ pip install -r requirements.pip
+```
+
+## Usage 
+
+```
+Usage: ride.py [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  login  Login to your Snapp account
+  price  Calculate price of a trip
+  ride   Compelete this function to take a ride with...
+```
+
+an example:
+
+```
+$ python ride.py ride --source 35.70071439856985 51.39509439468384 --destination 35.70364178950393 51.39932155609131
+```
---
+++ ride.py
@@ -0,0 +1,89 @@
+import os
+import codecs
+import subprocess
+import click
+
+from taxi import snapp
+
+
+config = '/home/vahid/.config/snapp-cli/'
+if not os.path.exists(config):
+    os.mkdir(config)
+
+
+def read_path():
+    sp, dp = None, None
+    path_file = open(config + 'path.csv', 'r')
+    path = path_file.read()
+
+    if path: 
+        return [(float(i), float(j)) for i,j in [d.split(',') for d in path.split('\n')[:2]]]
+
+    path_file.close()
+    return sp, dp
+
+sp, dp = read_path()
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--username', '-u', type=str)
+@click.option('--password', '-p', type=str)
+def login(username, password):
+    '''
+    Login to your Snapp account
+    '''
+    ok = snapp.login(username=username, password=password)
+    if ok:
+        click.echo('Successful login.')
+
+
+@click.command()
+@click.option('--source', '-s', nargs=2, type=float)
+@click.option('--destination', '-d', nargs=2, type=float)
+@click.option('--map', '-m', is_flag=True)
+def price(source, destination, map):
+    '''
+    Calculate price of a trip
+    '''
+    if map:
+        subprocess.call(['mapscii'])
+        source, destination = sp, dp
+    else:
+        pass
+    
+    r = snapp.ride(source[0], source[1], destination[0], destination[1])
+    click.echo("\n======================\n")
+    click.echo("Price: %d Rials\t Distance: %d" % r)
+    click.echo("\n======================\n")
+
+
+@click.command()
+def ride():
+    '''
+    Compelete this function to take a ride with snapp
+
+    Note: 
+    - Use chrome or firefox
+    - Please PEP8!!
+    - Dynamic path file
+
+    Your homework
+    '''
+    import time
+    click.echo('Waiting...')
+    time.sleep(5)
+    click.echo('Find a driver')
+
+
+cli.add_command(login)
+cli.add_command(price)
+cli.add_command(ride)
+
+
+if __name__ == '__main__':
+    cli()
