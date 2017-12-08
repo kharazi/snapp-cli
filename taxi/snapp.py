120e51b7b7a07221f9fdfad933d5f795b39c9f1b
kharazi <vahid@kharazi.net>
2017-12-08 00:00:54 +0000

    Initialize project: add Snapp API, project requirements and mapscii submodule

---
+++ taxi/snapp.py
@@ -0,0 +1,67 @@
+import os.path
+import shelve
+import requests
+
+
+c_path = '/home/vahid/.config/snapp-cli/cook'
+db = shelve.open(c_path)
+session = requests.session()
+
+cook, token = None, None
+
+
+if 'cook' in db.keys():
+    cook = db['cook']
+
+if 'token' in db.keys():
+    token = db['token']
+
+
+def save_session(session, token):
+    db['cook'] = requests.utils.dict_from_cookiejar(session.cookies)
+    db['token'] = token
+    db.sync()
+
+
+def ride(origin_lat, origin_lng, destination_lat, destination_lng, rount_trip=0, waiting=0):
+    data = {
+        'origin_lat': origin_lat,
+        'origin_lng': origin_lng,
+        'destination_lat': destination_lat,
+        'destination_lng': destination_lng,
+        'round_trip': rount_trip,
+        'waiting': waiting
+    }
+
+    headers = {
+        'Authorization': token
+    }
+
+    r = session.post(
+        'https://web-api.snapp.ir/api/v1/ride/price',
+        json=data,
+        cookies=cook,
+        headers=headers)
+    
+    if r.status_code >= 200 or r.status_code < 300:
+        res = r.json()['prices'][0]
+        return(res['final'], res['distance'])
+    else:
+        return False
+
+
+def login(username, password):
+    data = {
+        'username': username,
+        'password': password
+    }
+    r = session.post(
+        'https://web-api.snapp.ir/api/v1/auth/login',
+        json=data,
+        cookies=cook
+    )
+    if r.status_code >= 200 or r.status_code < 300:
+        save_session(session, token=r.json()['token'])
+        return True
+    else:
+        return False
